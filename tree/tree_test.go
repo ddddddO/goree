04d475165b442933b6e4ba55e21077d2cd7f8a00
ddddddO <lbfdeatq@gmail.com>
2020-12-09 00:35:13 +0900

    Add test

---
+++ tree/tree_test.go
@@ -0,0 +1,64 @@
+package tree
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+const testDir = "../testdata"
+
+func TestDeep(t *testing.T) {
+	tests := []struct {
+		name      string
+		targetDir string
+		isAll     bool
+		deepLevel int
+		buf       *bytes.Buffer
+
+		want string
+	}{
+		{
+			name:      "success 1",
+			targetDir: testDir,
+			isAll:     true,
+			deepLevel: 1,
+			buf:       &bytes.Buffer{},
+
+			want: genTestTree(`
+├── .a
+└── a
+`),
+		},
+		{
+			name:      "success 2",
+			targetDir: testDir,
+			isAll:     true,
+			deepLevel: 2,
+			buf:       &bytes.Buffer{},
+
+			want: genTestTree(`
+├── a
+│   ├── b
+│   └── .z
+└── .a
+`),
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			deep(tt.targetDir, tt.isAll, tt.deepLevel, 0, tt.buf)
+			got := tt.buf.String()
+			if got != tt.want {
+				t.Errorf("\nwant: \n%s\ngot: \n%s\n", tt.want, got)
+			}
+		})
+	}
+}
+
+func genTestTree(t string) string {
+	return strings.TrimPrefix(t, "\n")
+}
